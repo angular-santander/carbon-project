89950b7a463fe723ccd36d2313a482bde3911e2a
GiselleBarbosa <g88barbosa@gmail.com>
2022-10-26 15:46:46 -0300

    git init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { EditarCategoriaComponent } from './editar-categoria/editar-categoria.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EditarCategoriaComponent
+    
   ],
   imports: [
     BrowserModule
