6243638a383c61259341c34c4ec88931a9916079
GiselleBarbosa <g88barbosa@gmail.com>
2022-10-26 09:27:44 -0300

    edit created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EditarCategoriaComponent } from './editar-categoria/editar-categoria.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditarCategoriaComponent
   ],
   imports: [
     BrowserModule
