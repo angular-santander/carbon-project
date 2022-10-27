d91be9b1f5be9a5b3332178b67705a07c19670b8
GiselleBarbosa <g88barbosa@gmail.com>
2022-10-27 16:02:39 -0300

    modified style scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { CategoriasComponent } from './categorias/categorias.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'carbon-project';
