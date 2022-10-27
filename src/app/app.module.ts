224c286956a070641e3cbc453ea46b6670106c97
GiselleBarbosa <g88barbosa@gmail.com>
2022-10-27 08:27:02 -0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core'; //
 import { BrowserModule } from '@angular/platform-browser'; //
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
