f571e4459b9009707454fad904919e556c91a0bb
GiselleBarbosa <g88barbosa@gmail.com>
2022-11-03 10:43:34 -0300

    add button incluir e page incluir

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,17 +17,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'carbon-project'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('carbon-project');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('carbon-project app is running!');
-  });
 });
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,7 +6,7 @@ import { IconService, NavigationItem } from 'carbon-components-angular';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css'],
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
   @Input() headerItems: NavigationItem[];
