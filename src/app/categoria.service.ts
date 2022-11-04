8643c4207cacbd2fe929b262df9fc9f3420136bc
Izabela Soares <izabelasoares@MacBook-Air-de-Izabela.local>
2022-11-04 09:01:07 -0300

    adicionando logica do metodo POST categoria

---
+++ src/app/categoria.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+
+import { Categoria } from './categorias/categoria.model';
+import { HeaderComponent } from './header/header.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoriaService {
+  
+  private categoriasUrl = 'https://carbon-project.herokuapp.com/categorias'; //URL to web api
+  //http://localhost:4200/incluir
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json'})
+  };
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  addCategoria(categoria: Categoria): Observable<Categoria> {
+    return this.http.post<Categoria>(this.categoriasUrl, categoria, this.httpOptions).pipe(
+      catchError(this.handleError<Categoria>('addCategoria'))
+    );
+  }
+
+      /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   *
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      console.error('Aconteceu um erro!', error);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+}
--- src/app/categorias/incluir/incluir.component.ts
+++ src/app/categorias/incluir/incluir.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TableHeaderItem, TableItem, TableModel, InputModule } from 'carbon-components-angular';
+import { CategoriaService } from 'src/app/categoria.service';
+import { Categoria } from '../categoria.model';
 
 @Component({
   selector: 'app-incluir',
@@ -7,46 +9,29 @@ import { TableHeaderItem, TableItem, TableModel, InputModule } from 'carbon-comp
   styleUrls: ['./incluir.component.scss']
 })
 export class IncluirComponent implements OnInit {
+  categorias: Categoria[] = [];
 
   
-  constructor() { }
+  constructor(private categoriaService: CategoriaService) { }
   
-  title = 'Adicione um novo item';
-  description = 'insira o nome abaixo';
+  title = 'Você está no modo cadastro de categorias';
+  description = 'Insira o nome da categoria abaixo para realizar o cadastro';
 
-  tableModel = new TableModel();
-  tableData = [
-    [new TableItem({ data: '001' }), new TableItem({ data: 'Categoria 1' })],
-    [new TableItem({ data: '002' }), new TableItem({ data: 'Categoria 2' })],
-    [new TableItem({ data: '003' }), new TableItem({ data: 'Categoria 3' })],
-    [new TableItem({ data: '004' }), new TableItem({ data: 'Categoria 4' })],
-    [new TableItem({ data: '005' }), new TableItem({ data: 'Categoria 5' })],
-    [new TableItem({ data: '006' }), new TableItem({ data: 'Categoria 6' })],
-  ];
+
 
   ngOnInit() {
-    this.tableModel.data = [];
-    this.tableModel.header = [
-      new TableHeaderItem({ data: 'Id' }),
-      new TableHeaderItem({ data: 'Nome da Categoria' }),
-    ];
-    this.tableModel.pageLength = 2;
-    this.tableModel.currentPage = 1;
-    this.tableModel.totalDataLength = this.tableData.length;
-    this.selectPage(1);
+    
   }
-  selectPage(page) {
-    this.tableModel.currentPage = page;
-    const fullPage = [];
-    for (
-      let i = (page - 1) * this.tableModel.pageLength;
-      i < page * this.tableModel.pageLength &&
-      i < this.tableModel.totalDataLength;
-      i++
-    ) {
-      fullPage.push(this.tableData[0 + i]);
-      this.tableModel.data = fullPage;
+  
+  add(nome: string): void {
+    nome = nome.trim();
+    if(!nome) {return;}
+    this.categoriaService.addCategoria({ nome } as unknown as Categoria)
+      .subscribe(categoria => {
+        this.categorias.push(categoria);
+        console.log('teste add categoria:', categoria.id, categoria.nomeDaCategoria);
+      })
     }
-  }
+
 
 }
